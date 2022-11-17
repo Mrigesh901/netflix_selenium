1e7ea77c8b16a7b69db2b2ac935b586967871e4f
mrigesh901 <mrigeshpatni9@gmail.com>
2022-11-17 19:11:37 +0530

    Verify if video played

--- netflix_auomation.py
+++ netflix_auomation.py
@@ -24,7 +24,26 @@ starring = driver.find_element(by=By.XPATH, value='//*[@id="section-hero"]/div/d
 print("Star Cast    :     ", starring.text)
 
 driver.find_element(by=By.XPATH, value='//*[@id="section-additional-videos"]/div[2]/ul/li[1]/div/button/span[1]').click()
-time.sleep(60)
+
+while True:
+    try:
+        spinner  = driver.find_element(by=By.CSS_SELECTOR, value=".nf-loading-spinner")
+
+    except Exception as e:
+        try:
+            x = driver.find_element(by=By.ID, value='81624954')
+            count =  float(x.find_elements(by=By.CSS_SELECTOR, value="*")[0].get_attribute("currentTime"))
+            print(count)
+            if count>0:
+                print("Video Running")
+                # break
+            else:
+                print("Not Running")
+            
+            time.sleep(10)
+        except Exception as e:
+
+            print("video error : ", e)
 
 driver.quit()
 
